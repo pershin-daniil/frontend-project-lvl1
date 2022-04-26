4c1ffd2026f8573a163aff9094a33f099fb3c47e
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-04-26 20:30:09 +0300

    fix: edit cli.js and brain.js according to task

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,34 +1,33 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import greeting from '../src/cli.js';
 
-const isEven = (num) => (num % 2 === 0);
-
-const evenGame = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let i = 0;
-  while (i < 3) {
-    // Nothing said about range so that I use range from 0 to 99
-    const numberToAsk = Math.floor(Math.random() * 100);
-    console.log(`Question: ${numberToAsk}`);
-    const answer = readlineSync.question('Your answer: ');
-    let gameResult;
-    let correctAnswer;
-    if (isEven(numberToAsk)) {
-      gameResult = (answer.toLowerCase() === 'yes') ? 1 : 0;
-      correctAnswer = 'yes';
-    } else {
-      gameResult = (answer.toLowerCase() === 'no') ? 1 : 0;
-      correctAnswer = 'no';
-    }
-
-    if (gameResult === 1) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-      // i = 0;
-      break;
-    }
+const isEven = (num) => num % 2 === 0;
+const name = greeting();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let i = 0;
+while (i < 3) {
+  // Nothing said about range so that I use range from 0 to 99
+  const numberToAsk = Math.floor(Math.random() * 100);
+  console.log(`Question: ${numberToAsk}`);
+  const answer = readlineSync.question('Your answer: ');
+  let gameResult;
+  let correctAnswer;
+  if (isEven(numberToAsk)) {
+    gameResult = (answer.toLowerCase() === 'yes') ? 1 : 0;
+    correctAnswer = 'yes';
+  } else {
+    gameResult = (answer.toLowerCase() === 'no') ? 1 : 0;
+    correctAnswer = 'no';
   }
-  return (i === 3) ? console.log(`Congratulations, ${name}!`) : null;
-};
-export default evenGame;
+
+  if (gameResult === 1) {
+    console.log('Correct!');
+    i += 1;
+  } else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    // i = 0;
+    break;
+  }
+}
+if (i === 3) { console.log(`Congratulations, ${name}!`); }
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
 import greeting from '../src/cli.js';
-import evenGame from './brain-even.js';
 
-console.log('Welcome to the Brain Games!');
-
-let name = '';
-name = greeting();
-evenGame(name);
+greeting();
