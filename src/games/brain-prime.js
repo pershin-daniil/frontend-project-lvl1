263fd97301e78f5a791028116eab16ebbbef22cc
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-05-17 15:41:04 +0300

    fix: update logic brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,18 +1,20 @@
 import runGame from '../index.js';
 import getRandomInt from '../utilities.js';
 
-// eslint-disable-next-line max-len
-const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
-const isPrime = (num) => primeNumbers.includes(num);
+const isPrime = (num) => {
+  if (num <= 1 || Number.isInteger(num) === false) return false;
+  if (num === 2) return true;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
 const generateRound = () => {
-  const currentNum = getRandomInt(1, 100);
-  const question = `Question: ${currentNum}`;
-  if (isPrime(currentNum)) {
-    return ['yes', question];
-  }
-  return ['no', question];
+  const num = getRandomInt(1, 100);
+  const question = `Question: ${num}`;
+  const answer = isPrime(num) ? 'yes' : 'no';
+  return [answer, question];
 };
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
