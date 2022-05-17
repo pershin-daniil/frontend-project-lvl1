74c4bed150200f334ba44990fc2d94b278dd6364
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-05-17 13:48:56 +0300

    fix: update generateRound() for better maintainability

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,9 +4,10 @@ import getRandomInt from '../utilities.js';
 const isEven = (num) => num % 2 === 0;
 
 const generateRound = () => {
-  const currentNum = getRandomInt(1, 100);
-  const question = `Question: ${currentNum}`;
-  return isEven(currentNum) ? ['yes', question] : ['no', question];
+  const num = getRandomInt(1, 100);
+  const question = `Question: ${num}`;
+  const answer = isEven(num) ? 'yes' : 'no';
+  return [answer, question];
 };
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
