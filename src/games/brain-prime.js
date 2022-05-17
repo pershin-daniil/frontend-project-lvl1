387d9ee2c9585dd0bcd184887c10cf6b89e52320
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-05-17 17:52:51 +0300

    fix: update logic brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,9 +5,9 @@ const MIN_NUMBER = 1;
 const MAX_NUMBER = 100;
 
 const isPrime = (num) => {
-  if (num <= 1 || Number.isInteger(num) === false) return false;
-  if (num === 2) return true;
-  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+  if (num <= 1) return false;
+
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
   return true;
