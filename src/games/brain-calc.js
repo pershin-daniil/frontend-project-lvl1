34191a9722d698c02872f998a379a425ade4fd0e
Daniil Pershin <pershin.daniil.e@gmail.com>
2022-05-17 17:09:25 +0300

    fix: update logic brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -7,19 +7,13 @@ const operations = {
   '*': (a, b) => a * b,
 };
 
-const giveRandomExpressionAnswer = (num1, num2) => {
-  const operationsKeysArray = Object.keys(operations);
-  const operation = operationsKeysArray[getRandomInt(0, operationsKeysArray.length - 1)];
-
-  const result = String(operations[operation](num1, num2));
-
-  return [result, operation];
-};
-
 const generateRound = () => {
   const num1 = getRandomInt(1, 100);
   const num2 = getRandomInt(1, 100);
-  const [answer, operation] = giveRandomExpressionAnswer(num1, num2);
+  const operationsKeysArray = Object.keys(operations);
+  const operation = operationsKeysArray[getRandomInt(0, operationsKeysArray.length - 1)];
+
+  const answer = String(operations[operation](num1, num2));
   const question = `Question: ${num1} ${operation} ${num2}`;
   return [answer, question];
 };
